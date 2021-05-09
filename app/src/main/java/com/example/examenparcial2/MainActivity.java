2f582bd263b74fbd214fb824c902ce7b055b0386
Light-lig <lightcaceres@gmail.com>
2021-05-09 00:11:31 -0600

    App version 1.0

--- app/src/main/java/com/example/examenparcial2/MainActivity.java
+++ app/src/main/java/com/example/examenparcial2/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.examenparcial2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.examenparcial2.DB.AppDataBase;
+
+import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btnAgregar, btnReporte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAgregar = findViewById(R.id.btnAgregar);
+        btnReporte = findViewById(R.id.btnReporte);
+
+
+        btnAgregar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent agregarCompra = new Intent(MainActivity.this, compra.class);
+                startActivity(agregarCompra);
+            }
+        });
+        btnReporte.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent reporte = new Intent(MainActivity.this, Promedios.class);
+                startActivity(reporte);
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
